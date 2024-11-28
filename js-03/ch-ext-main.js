a61dd9350579f5aca7a781831d49e79b92d32f83
amoreno.ing <amoreno.ing@gmail.com>
2024-11-28 21:44:55 +0000

    Improves CH Extension pj

--- js-03/ch-ext-main.js
+++ js-03/ch-ext-main.js
@@ -1,14 +1,47 @@
 
-myLeads = []
+let myLeads = []
 
 const inputEl = document.querySelector('#input-el');
 const inputBtn = document.querySelector('#input-btn');
+const deleteBtn = document.querySelector('#delet-btn');
+const tabBtn = document.querySelector('#tab-btn');
 const ulEl = document.querySelector('#ulEl');
+
+// On load function
+if (localStorage.getItem('myLeads')) {
+    myLeads = JSON.parse(localStorage.getItem('myLeads'));
+    myLeads.forEach(renderUlwithInnerHTML);
+};
 
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
     renderUlwithInnerHTML(inputEl.value);
     inputEl.value = '';
+    //Update localStorage
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+});
+
+// On click tab
+tabBtn.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        renderUlwithInnerHTML(tabs[0].url);
+    })
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+});
+
+// Clear all leads
+deleteBtn.addEventListener('dblclick', () => {
+    localStorage.clear();
+    ulEl.innerHTML = '';
+    myLeads = [];
+});
+
+// New leed when enter
+inputEl.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        inputBtn.click();
+    }
 });
 
 function renderUl(text) {
@@ -20,8 +53,8 @@ function renderUl(text) {
 function renderUlwithInnerHTML(text) {
     const aText = `<a target="_blank" href="https://${text}">${text}</a>`;
     ulEl.innerHTML += tagText('li', aText);
-}
+};
 
 function tagText(tag, text){
     return `<${tag}>${text}</${tag}>`;
-}
+};
